9e077f7b6e04f3f1d4babd87fa099e7d9355f03d
chennamadhava <nag.chennamadhava123@gmail.com>
2019-06-17 12:23:30 +0530

    gv sjfdsljkgh lidsflgt

---
+++ src/main/java/programs/Add.java
@@ -0,0 +1,19 @@
+package programs;
+
+public class Add {
+	public int additionOfTwoNumbers() {
+
+		int a,b,c;
+		a=10;
+		b=20;
+		c=a+b;
+ System.out.println("The value of c is"+c);
+		return c;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Add a=new Add();
+    a.additionOfTwoNumbers();
+	}
+
+}
